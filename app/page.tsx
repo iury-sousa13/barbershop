d1b80f288ff60d64bfd5dafdc4abf62081c40124
IuryWemerson <iury_wemerson@hotmail.com>
2024-08-03 12:36:42 -0600

    chore: add shadcn lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image';
+import { Button } from './_components/ui/button';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return <Button>Hola</Button>;
 }
