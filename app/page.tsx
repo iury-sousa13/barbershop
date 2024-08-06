b35c700c8821b278a54b44d754b2d79ce9ed6b86
IuryWemerson <iury_wemerson@hotmail.com>
2024-08-05 20:39:36 -0600

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import { SearchIcon } from 'lucide-react';
+import { Header } from './_components/header';
 import { Button } from './_components/ui/button';
+import { Input } from './_components/ui/input';
+import Image from 'next/image';
 
 export default function Home() {
-  return <Button>Hola</Button>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl">
+          Olá, <strong>Iury</strong>
+        </h2>
+        <p className="mt-1 text-sm">Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="./banner-01.svg"
+            fill
+            className="rounded-xl object-cover"
+            alt="Agende com os melhores com FSW Baber"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
