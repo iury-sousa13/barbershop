53bc68bbe418893333e577831554474b57eaa0c0
IuryWemerson <iury_wemerson@hotmail.com>
2024-08-10 21:00:35 -0600

    feat: ensure only authenticate user can create book

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -132,7 +132,6 @@ export function ServiceItem({ service, barbershop }: ServiceItemProps) {
 
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       });
 
